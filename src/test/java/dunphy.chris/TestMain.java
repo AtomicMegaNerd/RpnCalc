1dad31f882398d617478cad3d112d372b4cdd29a
Chris Dunphy <chris@megaparsec.ca>
2020-05-11 08:41:10 -0600

    Add Maven and a basic unit test

---
+++ src/test/java/dunphy.chris/TestMain.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestMain {
+
+    @Test
+    public void testHookUp() {
+        assertEquals(3, 3);
+    }
+
+}
